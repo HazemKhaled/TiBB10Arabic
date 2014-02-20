0e7adee5c2741e26a318a8db5301250b18c9c33f
Hazem Khaled <hazem.khaled@gmail.com>
2014-02-20 13:03:02 +0400

    more components

---
+++ app/controllers/controller.js
@@ -0,0 +1,7 @@
+$.win.addAction('عربي تمام', 'ic_favorite.png', function(e) {
+	alert('good from controller');
+});
+
+$.win.addAction(L('from_i18n'), 'ic_feedback.png', function(e) {
+	alert('from i18 file');
+});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,23 @@
+///swipe dowm from top to see app menu. app menu is shown throughout all windows
+var appMenu = Ti.UI.BlackBerry.createApplicationMenu({
+	items : [{
+		title : "تجربة من الكنتروللر",
+		icon : "ic_help.png"
+	}, {
+		title : L("from_i18n"),
+		icon : "ic_home.png"
+	}]
+});
+
+appMenu.addEventListener("click", function(e) {
+	switch (e.index) {
+		case 0:
+			alert("from controller");
+			break;
+		case 1:
+			alert("from i18 file");
+			break;
+	}
+});
+
 $.index.open();
