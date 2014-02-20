99e03a280695f67df6d0c0956e13a1acee12a75d
Hazem Khaled <hazem.khaled@gmail.com>
2014-02-20 13:13:38 +0400

    text open file

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -5,3 +5,6 @@ $.win.addAction('عربي تمام', 'ic_favorite.png', function(e) {
 $.win.addAction(L('from_i18n'), 'ic_feedback.png', function(e) {
 	alert('from i18 file');
 });
+
+var file = Ti.Filesystem.getFile('app/native/assets/arabictxt.txt');
+$.textLbl.text = file.read().text;
